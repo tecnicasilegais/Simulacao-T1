647e8d77b19e4ebea3440bef60b840466db9b18a
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2021-04-02 15:21:34 -0300

    Created project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simulacao_T1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
